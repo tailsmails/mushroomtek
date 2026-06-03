ad7fa9f748b1c9b6c0a047d37d59e6f13026ed98
tailsmails <253711812+tailsmails@users.noreply.github.com>
2026-06-03 19:53:03 +0100

    dynamic switch between atci1 and pttynwcmd

--- mushroomtek.v
+++ mushroomtek.v
@@ -340,18 +340,16 @@ fn main() {
 	if os.exists('/dev/radio/pttynwcmd') {
 		active_modems << '/dev/radio/pttynwcmd'
 	}
-  else {
-		if os.exists('/dev/radio/atci1') {
-		  active_modems << '/dev/radio/atci1'
-	  }
-	  print('Protect atci2? (y/n): ')
-	  if os.exists('/dev/radio/atci2') && os.input('') == 'y' {
-		  active_modems << '/dev/radio/atci2'
-	  }
-	  if active_modems.len == 0 {
-		  println(term.red('No radio interfaces found.'))
-		  exit(1)
-	  }
+	else if os.exists('/dev/radio/atci1') {
+		active_modems << '/dev/radio/atci1'
+	}
+	print('Protect atci2(sim2)? (y/n): ')
+	if os.exists('/dev/radio/atci2') && os.input('') == 'y' {
+		active_modems << '/dev/radio/atci2'
+	}
+	if active_modems.len == 0 {
+		println(term.red('No radio interfaces found.'))
+		exit(1)
 	}
 
 	band_default := get_default_band(active_modems[0])
